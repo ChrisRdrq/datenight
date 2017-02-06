c8f9f3b0759a0cf220c0a11ad3bfa9ec18c564f4
philip frazer <philipfrazer@gmail.com>
2017-02-06 02:30:01 -0500

    Added edit and update methods to DatePostsController

--- datenight/app/controllers/date_posts_controller.rb
+++ datenight/app/controllers/date_posts_controller.rb
@@ -1,17 +1,19 @@
 class DatePostsController < ApplicationController
-
+  # index
   def index
 		@dates = DatePost.all
 	end
 
+  # show
   def show
    @date = DatePost.find(params[:id])
 	end
 
+  # new
   def new
 		@date = DatePost.new
 	end
-
+  # create
   def create
     @date = DatePost.new(datepost_params)
 
@@ -21,6 +23,22 @@ class DatePostsController < ApplicationController
       render 'new'
     end
   end
+
+  # edit
+  def edit
+    @date = DatePost.find(params[:id])
+  end
+  # update
+  def update
+    @date = DatePost.find(params[:id])
+
+    if @date.update(datepost_params)
+      redirect_to @date
+    else
+      render 'edit'
+    end
+  end
+
 
   private
     	def datepost_perams
